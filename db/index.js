242775eb97f400b1e2e1bf4d118beb449aed1398
Eric Park <epark0995@gmail.com>
2020-09-10 16:35:18 -0700

    changed mongo

--- db/index.js
+++ db/index.js
@@ -1,6 +1,1 @@
 module.exports = require('mongoose').connect(process.env.MONGODB_URI || 'mongodb://localhost/fitness_db', { useNewUrlParser: true, useUnifiedTopology: true })
-
-module.exports = require('mongoose').connect('mongodb://localhost/fitness_db', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
